1cf1254119683944ab6d8fc0f4405d802d73aa94
Pham Hanh <phamhanh@plumvillage.org>
2023-01-01 22:46:33 +0100

    hex

--- uploadHex.sh
+++ uploadHex.sh
@@ -1,1 +1,3 @@
-avrdude -c arduino -p atmega328p -P /dev/ttyACM0 -b 115200 -U flash:w:brewpi-avr.hex:i
+#!/bin/sh
+sudo killall --user brewpi
+avrdude -c arduino -p atmega328p -P /dev/ttyACM0 -b 115200 -U flash:w:brewpi.hex:i
